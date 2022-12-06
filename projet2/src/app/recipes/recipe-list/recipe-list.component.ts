9093915d7864692c8319f7975eb2ef1f9dadc31f
Kim Robert <kimrobert.nb@gmail.com>
2022-12-06 17:11:30 +0100

    commit - list of receipes basis

--- projet2/src/app/recipes/recipe-list/recipe-list.component.ts
+++ projet2/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
 
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'Just tryin...', 'https://cuisine.land/upload/recettes/large/53_MQGWWFCAA400CKDU.jpg')
+  ]
+
+  constructor() {}
+
+  ngOnInit() {}
 }
