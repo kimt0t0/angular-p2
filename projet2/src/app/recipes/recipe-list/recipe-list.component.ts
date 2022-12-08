2b17e8ed3e2b11ca2f51c931133eb699e0b40433
Kim Robert <kimrobert.nb@gmail.com>
2022-12-08 16:23:11 +0100

    fix - improved recipe details display and added formsmodule to app module

--- projet2/src/app/recipes/recipe-list/recipe-list.component.ts
+++ projet2/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,8 +11,8 @@ export class RecipeListComponent {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe 1', 'Just tryin...', 'https://cuisine.land/upload/recettes/large/53_MQGWWFCAA400CKDU.jpg'),
-    new Recipe('A Test Recipe 2', 'Just tryin...', 'https://cuisine.land/upload/recettes/large/53_MQGWWFCAA400CKDU.jpg'),
-    new Recipe('A Test Recipe 3', 'Just tryin...', 'https://cuisine.land/upload/recettes/large/53_MQGWWFCAA400CKDU.jpg')
+    new Recipe('A Test Recipe 2', 'Just tryin...', 'https://www.hachette.fr/sites/default/files/styles/vertical_recette/public/burger.jpg'),
+    new Recipe('A Test Recipe 3', 'Just tryin...', 'https://resize.elle.fr/portrait_320_webp/var/plain_site/storage/images/elle-a-table/recettes-de-cuisine/falafels-sauces-tahini-et-au-yaourt-2692913/46802554-1-fre-FR/Falafels-sauces-tahini-et-au-yaourt.jpg')
   ]
   recipe: {};
 
